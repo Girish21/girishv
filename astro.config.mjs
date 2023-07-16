4edb29b7256ebe4b5b7dab8e5b1aa2f572eb5ae5
Girish21 <girishkumarv21@gmail.com>
2023-07-16 13:22:14 +0530

    format astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
 
-import partytown from "@astrojs/partytown";
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap(), partytown()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }),
+    sitemap(),
+    partytown()
+  ],
   site: 'https://girishv.com',
   experimental: {
     assets: true
   }
-});
+})
