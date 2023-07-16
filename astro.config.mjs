5d65a2c15dabf79e80e67e7c575716d862b3caa3
Girish21 <girishkumarv21@gmail.com>
2023-07-16 13:20:43 +0530

    fix webpage test suggestions https://url.vgirish.net/0BPyj8FOuI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,16 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
-import sitemap from '@astrojs/sitemap'
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false
-    }),
-    sitemap()
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sitemap(), partytown()],
   site: 'https://girishv.com',
   experimental: {
     assets: true
   }
-})
+});
